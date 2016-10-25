0360b4cb1daf69e418d2aa457b118d8d67101c05
kawase-y <lovol0919@gmail.com>
2016-10-25 16:19:32 +0900

    login周り

--- login.lua
+++ login.lua
@@ -31,7 +31,7 @@ local backBtn 					-- 戻るボタン
     -- ログインAPIのリクエストを送る    local reqbody = "email="..emailText.."&password="..passwordText
     local respbody = {}
     local body, code, headers, status = http.request{
-        url = "http://questionboardweb.herokuapp.com/api/v1/auth/sign_in",
+        url = "http://localhost:3000/api/v1/auth/sign_in",
         method = "POST",
         headers =
         {
@@ -50,8 +50,6 @@ local backBtn 					-- 戻るボタン
     userInfo["Client"]      = headers["client"]
 
 
-local user = json.decode(table.concat(respbody))["data"]
-print(user["id"])
 
 end
 
